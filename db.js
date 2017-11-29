572722699eed61fa017695ae515ebbce6e14d02f
thmsdnnr <thmsdnnr@gmail.com>
2017-11-29 11:00:23 -0600

    now with db persistence

---
+++ db.js
@@ -0,0 +1,68 @@
+var MongoClient = require('mongodb').MongoClient;
+
+let db;
+let W;
+let dbUrl=process.env.PROD_DB||'mongodb://localhost:27017/';
+
+function connect(callback) {
+  if (db===undefined) {
+    MongoClient.connect(dbUrl, function(err, database){
+      if(err) { return callback(err) };
+      db=database;
+      callback(null, db);
+    });
+  }
+  else { callback(null, db); }
+}
+
+connect(function(d){ });
+
+//word lists are tied to word list names
+/*
+word list:
+  WORDS []String
+  NUMWORDS num
+  SOURCE String
+  AUTHOR String
+  TAGS: []String
+*/
+
+//right now don't exclude duplicates
+//can save word lists and can save boards with positions
+
+//one room has: one owner, one board, one wordlist
+//can share with friends
+
+//if you created a board, you can enable/disable shared editing
+//if you created a board, you can limit actions other users can perform on your movable board:
+// ->read (view board as it moves in real-time)
+// ->write (add elements/delete elements)
+//->maybe delete elements w/ timer, or can only delete + create a certain #
+// ->execute (delete elements, reset board entirely)
+
+/*Word Lists*/
+exports.saveRoom = function(D, cb) {
+  db.collection("rooms").update({room:D.room}, {room:D.room, data:D.data, createdOn: new Date()}, {upsert:true},
+  function(err, data) { cb(err, data); });
+}
+
+exports.loadRoom = function(name, cb) {
+  db.collection("rooms").find({room:name}).toArray(function(err,data){
+    cb(err, data);
+  });
+}
+
+exports.saveWordList = function(data, cb) {
+  const createdOn=new Date();
+  const { words, numWords, source, author, tags } = data;
+  db.collection("wordLists").insert({name, words, numWords, source, author, tags, createdOn}, function(err,data){
+    if(!err) { cb(data); }
+    else { cb(false); }
+  });
+}
+
+exports.getWordList = function(data,cb) {
+  db.collection("wordLists").find({name:data.name}).sort({createdOn:-1}).toArray(function(err, docs) {
+  if(!err) { (docs.length) ? cb(docs) : cb(false); }
+  });
+}
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express=require('express');
 // const MongoStore = require('connect-mongo')(session);
+const path=require('path');
 const http=require('http');
 const WebSocket=require('ws');
 const appPORT=process.env.PORT || 8080;
 const app=express();
+const Db=require('./db.js');
+let ADJS=["divine","elegant","classy","winning","winsome","comely","luring","pretty","radiant","siren","angelic","sublime","refined","lovable","darling","magical","sweet"];;
+let ANIMALS=["zebra","cat","gazelle","raccoon","yak","otter","hyena","puma"];
+app.use('/static',express.static(path.join(__dirname,'/static')));
 
 const server=http.createServer(app);
 const wss=new WebSocket.Server({server: server});
@@ -13,47 +18,123 @@ function randHex() { return parseInt(Math.floor(Math.random()*255)).toString(16)
 
 function rIn() { return Math.floor(Math.random()*255); }
 
+function rArr(arr) {
+  return arr[Math.floor(Math.random()*(arr.length-1))];
+}
+
 function randomRGB() {
   // return `#${randHex()}${randHex()}${randHex()}`;
   return `rgba(${rIn()},${rIn()},${rIn()},1.0)`;
 }
 
-function randomString(idx) {
-  return ['marmot','comet','test','other','curious','animals'][idx];
+function randomString(idx) { //TODO client names
+  return rArr(ADJS)+" "+rArr(ANIMALS);
+  // return ['marmot','comet','test','other','curious','animals'][idx];
 }
 
+const remove = (arr, ele) => arr.filter(e=>e!==ele);
+
+let clientID=0;
+let clients=[]; //map of clientID to WS Client Object
 let clientColors={};
 let clientNames={};
+let rooms={}; //map of roomID to an array of clientIDs joined to room
 
-wss.broadcast = function broadcast(data, sendingClient) {
-  wss.clients.forEach(function each(client) {
-    if (client._ultron.id!==sendingClient) { client.send(data); }
+app.get('/t/:p', function(req,res) {
+  console.log(req.params.p);
+  // res.end(JSON.stringify({msg:'done'}));
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+wss.broadcast = function broadcast(data, room, sendingClient) { //TODO "rooms" based on word sets
+  rooms[room].forEach(function(clientID) {
+    if (clientID!==sendingClient) {
+      let client=clients[clientID];
+      if (client&&client.readyState===client.OPEN) { client.send(data); }
+      else {
+        //client ID is stale, remove from rooms
+        console.log('clean id', rooms, rooms[room]);
+        rooms[room] = remove(rooms[room],clientID);
+        console.log('clean id', clientID);
+      }
+    }
   });
 };
 
 wss.on('connection', function connection(client) {
-  console.log('new connection client: ',client._ultron.id);
-  clientColors[client._ultron.id]=randomRGB();
-  clientNames[client._ultron.id]=randomString(client._ultron.id);
-  if (client.readyState === client.OPEN) {
+  clientID++;
+  let CUID=clientID;//client._ultron.id;
+  clients[clientID]=client;
+  // clients.push({id:clientID, client:client, room:room});
+  console.log('new connection client: ',CUID);
+  clientColors[CUID]=randomRGB();
+  clientNames[CUID]=randomString(CUID);
+  if (client.readyState===client.OPEN) {
     client.on('message', function incoming(message) {
       if (message) {
         message=JSON.parse(message);
         console.log(message);
       }
+      if (!['hydrate','newClient','freeze','moving'].includes(message.action)) {
+        Db.saveRoom({room:message.room, data:message.data}, function(err, data) {
+          console.log(data.result,'saveroom');
+        });
+      }
       switch(message.action) {
-        case 'newClient': client.send(JSON.stringify({msg:'oheythere'})); break;
-        case 'moving': wss.broadcast(JSON.stringify(message),client._ultron.id); break;
+        case 'stateUpdate':
+          console.log('serverstateupdate msg data',message.data);
+          if (!message.data.length) { break; }
+          else {
+            wss.broadcast(JSON.stringify({action:'hydrate',data:message.data}),message.room, CUID);
+            Db.saveRoom({room:message.room, data:message.data}, function(err, data) {
+            console.log(data.result,'saveroom');
+          });
+        }
+          // console.log(message);
+          break;
+        case 'newClient':
+          //see if room name exists in the dB
+          //if so, push room data
+          //else don't.
+          client._CURRENT_ROOM=message.room;
+          if (rooms[message.room]&&rooms[message.room].length) { //request room state from current client and update Db
+            console.log('m',rooms[message.room])
+            wss.broadcast(JSON.stringify({action:'requestCurrentState'}), message.room, null); //TODO request from most recent client with an action
+          }
+          else { //no one in the room, get last state from Db
+            Db.loadRoom(message.room, function(err, data) {
+              console.log(err);
+              console.log('load room');
+                if (data.length) {
+                  data=data[0];
+                  client.send(JSON.stringify({action:'hydrate',data:data}));
+                } else { //new room entirely make client owner
+                  client.send(JSON.stringify({action:'newRoom'}));
+                }
+            });
+          }
+          //add client to room
+          rooms[message.room] ? rooms[message.room].push(clientID) : rooms[message.room]=[clientID];
+          client.send(JSON.stringify({msg:'oheythere', id:clientID}));
+          break;
+        case 'moving': wss.broadcast(JSON.stringify(message),message.room,CUID); break;
         case 'freeze':
           wss.broadcast(
             JSON.stringify(
-              Object.assign(message,{clientName:clientNames[client._ultron.id], color:clientColors[client._ultron.id]})),
-              client._ultron.id
+              Object.assign(message,{clientName:clientNames[CUID], color:clientColors[CUID]})),
+              message.room, CUID
             ); break;
-        case 'unfreeze': wss.broadcast(JSON.stringify(message),client._ultron.id); break;
-        case 'delete': wss.broadcast(JSON.stringify(message),client._ultron.id); break;
-        default: wss.broadcast(JSON.stringify(message),client._ultron.id); break;
+        case 'unfreeze': wss.broadcast(JSON.stringify(message),message.room,CUID); break;
+        case 'delete': wss.broadcast(JSON.stringify(message),message.room,CUID); break;
+        default: wss.broadcast(JSON.stringify(message),message.room,CUID); break;
       }
     });
   }
+  client.on('close', function close() {
+  		 console.log(CUID,"DISCONNECTED");
+       console.log('room',rooms[client._CURRENT_ROOM]);
+       rooms[client._CURRENT_ROOM]=remove(rooms[client._CURRENT_ROOM],CUID);
+       console.log('room',rooms[client._CURRENT_ROOM]);
+       delete clients[CUID];
+  	});
 });
