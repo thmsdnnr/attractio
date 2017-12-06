d7bacbb5a6e739beba2efcc96d3534fe986f3a67
thmsdnnr <thmsdnnr@gmail.com>
2017-12-06 11:08:43 -0600

    codebase cleanup

--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
+const $={
+  wsPingInterval:15000 //ms
+};
+
 const ADJS=["divine","elegant","classy","winning","winsome","comely","luring","pretty","radiant","siren","angelic","sublime","refined","lovable","darling","magical","sweet"];;
 const ANIMALS=["zebra","cat","gazelle","raccoon","yak","otter","hyena","puma"];
 const URL_ADJS=["elastic","elated","elfin","elite","enchanted","endurable","exuberant","exultant","fabulous","fearless","fierce","hallowed","immense","jazzy","lavish","luxuriant","lyrical","melodic","nimble","oceanic","optimal","placid","quirky","sassy","savory","spiffy","swanky","ultra"];
@@ -10,7 +14,7 @@ const randomRGB = () => `rgba(${rIn()},${rIn()},${rIn()},1.0)`
 const randomString = (idx) =>rArr(ADJS)+" "+rArr(ANIMALS);
 const remove = (arr, ele) => {
   if (!(arr&&Array.isArray(arr))||(!(ele&&Number.isInteger(ele)))) { return null; }
-  return arr.indexOf(ele)!==-1 ? arr.filter(e=>e!==ele) : null;
+  return arr.indexOf(ele)!==-1 ? arr.filter(e=>e!==ele) : arr;
 }
 const promote = (arr, ele) => {
   if (!arr||!ele) { return false; }
@@ -19,4 +23,4 @@ const promote = (arr, ele) => {
   return (idx!==-1) ? [ele].concat(arr.slice(0,idx),arr.slice(idx+1)) : arr;
 }
 
-module.exports = { randHex, rIn, rArr, randomURL, randomRGB, randomString, remove, promote};
+module.exports = { $, randHex, rIn, rArr, randomURL, randomRGB, randomString, remove, promote};
--- ws.js
+++ ws.js
@@ -3,26 +3,23 @@ const dQueries=require('./db.rooms.js');
 const UTILS=require('./utils.js');
 
 exports.initServer = (server, dbCon) => {
+  const dbActionsToIgnore=['emptyRoom','newClient','lapsedClient','freeze','moving'];
   const Db=dQueries(dbCon);
-
-  let wss;
   let clientID=0;
   let clients={}; //map of clientID to WS Client Object
-  let clientColors={};
-  let clientNames={};
   let rooms={}; //map of roomID to an array of clientIDs joined to room
 
   const sendPrivateMessage = (msg, recClient) => {
-    let C=clients[recClient];
+    let C=clients[recClient].obj;
     if (C) { C.sendMsg(msg); }
     else { return false; }
   };
-  
-  wss=new WebSocket.Server({server: server});
+
+  let wss=new WebSocket.Server({server: server});
   wss.broadcast = function broadcast(data, room, sendingClient) {
     rooms[room].forEach(function(clientID) {
       if (clientID!==sendingClient) {
-        let client=clients[clientID];
+        let client=clients[clientID].obj;
         if (client&&client.readyState===client.OPEN) { client.sendMsg(data); }
         else { //client ID is stale, remove from rooms
           rooms[room] = remove(rooms[room],clientID);
@@ -31,12 +28,32 @@ exports.initServer = (server, dbCon) => {
     });
   };
 
+  function heartbeat() { this.isAlive = true; }
+  const interval = setInterval(function ping() {
+    Object.keys(clients).forEach(function each(clientID) {
+      let client = clients[clientID];
+      let cObj=client.obj;
+      if (cObj.isAlive === false) { terminateClient(client); }
+      cObj.isAlive=false;
+      cObj.ping('', false, true);
+      });
+    }, UTILS.$.wsPingInterval);
+
+  function terminateClient(client) {
+    let cObj=client.obj;
+    let CUID=client.id;
+    cObj.terminate();
+    let R=cObj._CURRENT_ROOM;
+    if (R && rooms[R]) { rooms[R]=UTILS.remove(rooms[R],CUID); }
+    delete clients[CUID];
+  }
+
   wss.on('connection', function connection(client) {
+    client.isAlive = true;
+    client.on('pong', heartbeat.bind(client));
     clientID++;
     let CUID=clientID;
-    clients[clientID]=client;
-    clientColors[CUID]=UTILS.randomRGB();
-    clientNames[CUID]=UTILS.randomString(CUID);
+    clients[clientID]={obj: client, color:UTILS.randomRGB(), name:UTILS.randomString(CUID), id:clientID};
     if (client.readyState===client.OPEN) {
       client.sendMsg = function(message) { //Server Message Bus
         // console.log('server sent msg to client', JSON.stringify(message).slice(0,100), CUID);
@@ -44,7 +61,6 @@ exports.initServer = (server, dbCon) => {
         client.send(payload);
       };
       client.on('message', function incoming(message) {
-        //when we receive a message from a client, put them at the beginning of the clients array for that room.
         if (!message) { return false; }
         message=JSON.parse(message);
         if (client._CURRENT_ROOM&&message.data) {
@@ -52,7 +68,7 @@ exports.initServer = (server, dbCon) => {
           rooms[R]=UTILS.promote(rooms[R], CUID);
         }
         // console.log('server received message from client', JSON.stringify(message).slice(0,100));
-        if (!['hydrate','newClient','freeze','moving','stateUpdate'].includes(message.action)) {
+        if (!dbActionsToIgnore.includes(message.action)) {
           Db.saveRoom({room:message.room, data:message.data}, function(err, data) {
           });
         }
@@ -75,10 +91,7 @@ exports.initServer = (server, dbCon) => {
                 let urls=[UTILS.randomURL(), UTILS.randomURL(), UTILS.randomURL()];
                 client.sendMsg({action:'roomFull', data:urls}); return false;
               }
-              /*We request the state from the client with the most recent activity in the room
-              that also has data: we Promote the client ID to the front of a queue each action */
               sendPrivateMessage({action:'requestCurrentState'},rooms[R][0]);
-              //TODO need to have a thing to do in case no response is given: setTimeout to dB call or call next up client
             }
             else { //no one in the room, get last state from Db
               Db.loadRoom(message.room, function(err, data) {
@@ -89,41 +102,17 @@ exports.initServer = (server, dbCon) => {
                   }
               });
             }
-            //add client to room
-            rooms[R] = rooms[R] ? rooms[R].concat(CUID) : [CUID];
-            client.sendMsg({msg:'Hello New Client', id:clientID});
-            break;
-          case 'lapsedClient':
-            // console.log('lapsed client'); //TODO better lapsed client handling
-            client._CURRENT_ROOM=R;
-            if (rooms[R]&&rooms[R].length) {
-              sendPrivateMessage({action:'requestCurrentState'},rooms[R][0]);
-              //TODO need to have a thing to do in case no response is given
-            }
-            else { //no one in the room, get last state from Db
-              Db.loadRoom(message.room, function(err, data) {
-                  if (data.length&&data[0].data) {
-                    client.sendMsg({action:'hydrate',data:data[0].data});
-                  } else { //new room entirely, make client owner
-                    client.sendMsg({action:'newRoom'});
-                  }
-              });
-            }
             rooms[R] = rooms[R] ? rooms[R].concat(CUID) : [CUID];
             client.sendMsg({msg:'Hello New Client', id:clientID});
             break;
           case 'moving': wss.broadcast(message,message.room,CUID); break;
-          case 'freeze': wss.broadcast(Object.assign(message,{clientName:clientNames[CUID], color:clientColors[CUID]}),message.room, CUID); break;
+          case 'freeze': wss.broadcast(Object.assign(message,{clientName:clients[CUID].name, color:clients[CUID].color}),message.room, CUID); break;
           case 'unfreeze': wss.broadcast(message,message.room,CUID); break;
           case 'delete': wss.broadcast(message,message.room,CUID); break;
           default: wss.broadcast(message,message.room,CUID); break;
         }
       });
     }
-    client.on('close', function close() {
-      let R=client._CURRENT_ROOM;
-         if (R && rooms[R]) { rooms[R]=UTILS.remove(rooms[R],CUID); }
-         delete clients[CUID];
-    	});
+    client.on('close', function close() { terminateClient(clients[CUID]); });
   });
 }
